ad2a8dff7b88d127db984aa289f2c2be9bccf020
edmilson.langa <edmilson.langa@accenture.com>
2018-07-19 11:07:39 +0200

    AppOne update

--- AssignmentOne.Edmilson/src/Main.java
+++ AssignmentOne.Edmilson/src/Main.java
@@ -16,42 +16,31 @@ public class Main {
         GregorianCalendar time = new GregorianCalendar();
 
         int hour = time.get(Calendar.HOUR_OF_DAY);
-        int min = time.get(Calendar.MINUTE);
-        int day = time.get(Calendar.DAY_OF_MONTH);
-        int month = time.get(Calendar.MONTH)+1;
-        int year = time.get(Calendar.YEAR);
 
-        System.out.println("The current time is \t" +hour + ":" + min);
-        System.out.println("Today's date is \t " + month + "/" + day + "/" + year);
-
-        if (hour < 12)
-            System.out.println("Good Morning!" +" "+username);
-        else if (hour<17 && !(hour == 12))
-            System.out.println("Good Afternoon"+" "+username);
-        else if (hour == 12)
-            System.out.println("Good Noon"+" "+username);
-        else
-            System.out.println("Good Evening"+username);
-
+        if (hour < 12) {
+            System.out.println("Good Morning!" + " " + username);
+        } else if (hour < 17) {
+            System.out.println("Good Afternoon" + " " + username);
+        } else
+            System.out.println("Good Evening" + username);
 
 
         System.out.println("Please Kindly Enter Your Age: ");
         int age = sc.nextInt();
 
-        int randomNumber =(int)(Math.random()*(90-18)+1);
+        int randomNumber = (int) (Math.random() * (90 - 18) + 18);
         int difference;
 
-        if (randomNumber > age ) {
+        if (randomNumber > age) {
             difference = randomNumber - age;
-            System.out.println("I am: " +" "+ randomNumber +" "+"Years old which is" + " "+difference+" "+ "Years older than you.");
-        }
-        else if ( randomNumber < age ) {
+            System.out.println("I am: " + " " + randomNumber + " " + "Years old which is" + " " + difference + " " + "Years older than you.");
+        } else if (randomNumber < age) {
             difference = age - randomNumber;
             System.out.println("I am: " + " " + randomNumber + " " + "Years old, which is" + " " + difference + " " + "Years younger than you");
         }
 
-        System.out.println("Twice my age would be" +" " + randomNumber*2 );
-        if (randomNumber%2 == 0 )
+        System.out.println("Twice my age would be" + " " + randomNumber * 2);
+        if (randomNumber % 2 == 0)
             System.out.println("My age is an Even number");
 
         else
